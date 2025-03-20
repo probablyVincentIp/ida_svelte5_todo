c0fe6476d5196d7a672ab312cb53f016c53e6db1
Vincent Ip <vincent.ip@ida.me>
2025-03-20 15:44:50 +0100

    setup todo app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<ul class="ul">
+	<li><a href="todo">todo</a></li>
+	<li><a href="todo-kit">todo-kit</a></li>
+</ul>
---
+++ src/routes/todo-kit/+page.svelte
@@ -0,0 +1,7 @@
+<main>
+	<h1 class="text-center text-2xl font-bold text-gray-800 md:text-3xl">Server Side Todo App</h1>
+
+	<!-- todo input form -->
+
+	<!-- todo list -->
+</main>
